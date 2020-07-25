6ec2a4286378280e800f1b8c0662e71f89dd6dc7
kdijulio-ITH <kdijulio@intouchhealth.com>
2020-07-25 16:45:44 -0300

    entrega proyecto3

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ReactASD</title>
+    <title>SPA PERFUMES</title>
     <link href="{{ asset('css/app.css')}}" rel="stylesheet"></link>
 
   </head>
